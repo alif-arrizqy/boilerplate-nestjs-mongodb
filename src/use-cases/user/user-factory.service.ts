b013a9f93f1047d83dcdc828acb3e1d6ed10d454
alif-arrizqy <alif.arrizqy20@gmail.com>
2024-01-01 12:14:28 +0700

    Refactor phone number formatting in UserFactoryService

--- src/use-cases/user/user-factory.service.ts
+++ src/use-cases/user/user-factory.service.ts
@@ -6,24 +6,45 @@ import { User } from 'src/core/entities';
 export class UserFactoryService {
   createNewUser(createUserDto: CreateUserDto) {
     const newUser = new User();
-    newUser.username = createUserDto.username;
-    newUser.password = createUserDto.password;
     newUser.firstname = createUserDto.firstname;
     newUser.lastname = createUserDto.lastname;
-    newUser.phone = createUserDto.phone;
+    // check if phone number start with 0
+    if (createUserDto.phone.startsWith('0')) {
+      // remove 0 from phone number
+      const formatPhoneNumber = createUserDto.phone.replace(/^0+/, '');
+      // add 62 to phone number
+      const newFormatPhoneNumber = `62${formatPhoneNumber}`;
+      newUser.phone = newFormatPhoneNumber;
+    }
+    // check if phone number start with 62
+    else if (createUserDto.phone.startsWith('62')) {
+      newUser.phone = createUserDto.phone;
+    } else {
+      const newFormatPhoneNumber = `62${createUserDto.phone}`;
+      newUser.phone = newFormatPhoneNumber;
+    }
     newUser.email = createUserDto.email;
+    newUser.password = createUserDto.password;
     newUser.role = createUserDto.role;
     return newUser;
   }
 
   updateUser(updateUserDto: UpdateUserDto) {
     const newUser = new User();
-    newUser.username = updateUserDto.username;
-    newUser.password = updateUserDto.password;
     newUser.firstname = updateUserDto.firstname;
     newUser.lastname = updateUserDto.lastname;
-    newUser.phone = updateUserDto.phone;
+    if (updateUserDto.phone.startsWith('0')) {
+      const formatPhoneNumber = updateUserDto.phone.replace(/^0+/, '');
+      const newFormatPhoneNumber = `62${formatPhoneNumber}`;
+      newUser.phone = newFormatPhoneNumber;
+    } else if (updateUserDto.phone.startsWith('62')) {
+      newUser.phone = updateUserDto.phone;
+    } else {
+      const newFormatPhoneNumber = `62${updateUserDto.phone}`;
+      newUser.phone = newFormatPhoneNumber;
+    }
     newUser.email = updateUserDto.email;
+    newUser.password = updateUserDto.password;
     newUser.role = updateUserDto.role;
     return newUser;
   }
